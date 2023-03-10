80d747fc17543b5495b1abfc156cd0585660ed84
Thierry Castiglioni <castth@hotmail.com>
2023-03-10 18:42:11 +0100

    select calculator + css grid

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ let spent = 0;
 let tries = document.getElementById('tries').value;
 let triesInput = document.getElementById('tries');
 
-function recalculateSpent() {
+// Calculate the total cost from 0
+
+function calculateSpent() {
   spent = 0;
   for (let i = 1; i <= tries; i++) {
     spent += i * 100;
@@ -12,7 +14,28 @@ function recalculateSpent() {
 
 triesInput.addEventListener('input', function() {
   tries = this.value;
-  recalculateSpent();
+  calculateSpent();
 });
 
-recalculateSpent();
+// Calculate the cost for a chosen selection
+
+let spentSelect = 0;
+let startingInput = document.getElementById('startingTry');
+let endingInput = document.getElementById('endingTry');
+let calculateSelection = document.getElementById('calculateSelection');
+
+function calculateSelect() {
+  let startingTry = parseInt(startingInput.value);
+  let endingTry = parseInt(endingInput.value);
+  spentSelect = 0;
+  for (let i = 0; i <= endingTry - startingTry; i++) {
+    spentSelect += (startingTry + i) * 100;
+  }
+  resultSelect.textContent = spentSelect;
+}
+
+calculateSelection.addEventListener('click', function() {
+  calculateSelect();
+});
+
+calculateSpent();
