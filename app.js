b69269307afd67c1cd57ca95b7287314dce2bc7b
Thierry Castiglioni <castth@hotmail.com>
2023-03-12 13:42:33 +0100

    JS tweak

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-let spent = 0;
-let tries = document.getElementById('tries').value;
-let triesInput = document.getElementById('tries');
-
 // Calculate the total cost from 0
+
+let spent = 0;
+let triesInput = document.getElementById('tries').value;
 
 function calculateSpent() {
   spent = 0;
-  for (let i = 1; i <= tries; i++) {
+  for (let i = 1; i <= triesInput; i++) {
     spent += i * 100;
   }
   result.textContent = spent;
 }
 
-triesInput.addEventListener('input', function() {
-  tries = this.value;
+tries.addEventListener('input', function() {
+  triesInput = parseInt(this.value);
   calculateSpent();
 });
 
@@ -37,5 +36,3 @@ function calculateSelect() {
 calculateSelection.addEventListener('click', function() {
   calculateSelect();
 });
-
-calculateSpent();
